f0908f20b84bba4478ec570414f1b2d9bdd91bf8
Bunkro <palerms@tutanota.com>
2020-01-01 04:01:40 -0600

    animation icon

--- dist/js/function.js
+++ dist/js/function.js
@@ -2,9 +2,12 @@
 
 var sideWidth = document.getElementById('dash');
 var fire = document.getElementById('leftSide');
+var icon = document.getElementById('iconPanel');
+
 
 function navToogle() {
-  sideWidth.classList.toggle("l-grid-action")
+  sideWidth.classList.toggle("l-grid-action");
+  icon.classList.toggle("l-icon-panel");
 }
 
 fire.addEventListener("click", navToogle)

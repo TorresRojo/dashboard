1af78843539185530a129af03b0b2b4aa50233bf
Bunkro <palerms@tutanota.com>
2020-01-01 02:42:25 -0600

    apply main function

--- dist/js/function.js
+++ dist/js/function.js
@@ -1,11 +1,10 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-  document.getElementById("leftSide").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+
+
+var sideWidth = document.getElementById('dash');
+var fire = document.getElementById('leftSide');
+
+function navToogle() {
+  sideWidth.classList.toggle("l-grid-action")
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-}
+fire.addEventListener("click", navToogle)

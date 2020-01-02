d3a7fb0cad7a0558ea2487d6a83ff086508f31fa
Bunkro <palerms@tutanota.com>
2020-01-01 21:57:03 -0600

    config inner elements

--- dist/js/function.js
+++ dist/js/function.js
@@ -2,12 +2,25 @@
 
 var sideWidth = document.getElementById('dash');
 var fire = document.getElementById('leftSide');
-var icon = document.getElementById('iconPanel');
-
+var iconRotate = document.getElementById('iconPanel');
+var panelHide = document.getElementsByClassName('l-item__description');
+var boxPanel = document.getElementsByClassName('l-content__item');
 
 function navToogle() {
   sideWidth.classList.toggle("l-grid-action");
-  icon.classList.toggle("l-icon-panel");
+  iconRotate.classList.toggle("l-icon-panel");
+  var i;
+  for (i = 0; i < panelHide.length; i++) {
+    var inner = panelHide[i];
+    inner.classList.toggle("u-hide");
+  }
+  var e;
+  for (e = 0; e < boxPanel.length; e++) {
+     var middleFix = boxPanel[e];
+       middleFix.classList.toggle("l-fix-flex");
+  }
+
 }
+
 
 fire.addEventListener("click", navToogle)
